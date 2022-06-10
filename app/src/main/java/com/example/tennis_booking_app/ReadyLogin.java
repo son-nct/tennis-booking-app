7315cffa9538f0385822e4859d0652ccc08d61ca
phuchlh <hongphucthd@gmail.com>
2022-06-10 15:52:05 +0700

    [PhucHLH] re-upload 3 screens

--- app/src/main/java/com/example/tennis_booking_app/ReadyLogin.java
+++ app/src/main/java/com/example/tennis_booking_app/ReadyLogin.java
@@ -3,12 +3,28 @@ package com.example.tennis_booking_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class ReadyLogin extends AppCompatActivity {
 
+    RelativeLayout layoutWait, layoutSelect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ready_login);
+        layoutWait = (RelativeLayout) findViewById(R.id.layoutWaiting);
+        layoutSelect = (RelativeLayout) findViewById(R.id.layoutSelect);
+
+        layoutSelect.setVisibility(View.GONE);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                layoutWait.setVisibility(View.GONE);
+                layoutSelect.setVisibility(View.VISIBLE);
+            }
+        },1000);
     }
 }
