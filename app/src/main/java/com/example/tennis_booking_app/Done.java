44107ede1b775a14ee6cc2e980c1b52f879e3ad1
thinhtran2478 <thinhtran2478@gmail.com>
2022-06-14 13:37:26 +0700

    [thinhltd][finished task design interface mobile] 1.Details_Promotion 2.Done 3.Promotion 4.Register 5.Retrieval 6.Send_feedback 7.Sign_up_success 8.Starts

---
+++ app/src/main/java/com/example/tennis_booking_app/Done.java
@@ -0,0 +1,14 @@
+package com.example.tennis_booking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Done extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+    }
+}
