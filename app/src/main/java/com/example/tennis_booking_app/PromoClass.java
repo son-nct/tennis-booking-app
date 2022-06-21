a1361179ce58997043ddcaaeb2957c51c7c24d90
SE140774\Administrator <truong>
2022-06-21 23:39:47 +0700

    [truongnl][fix promo]

---
+++ app/src/main/java/com/example/tennis_booking_app/PromoClass.java
@@ -0,0 +1,72 @@
+package com.example.copy;
+
+public class PromoClass {
+    private int hinh,sao,the;
+    private String tenSan,khoangCach,promo,maKM;
+
+    public PromoClass(int hinh, int sao, int the, String promo, String tenSan, String khoangCach, String maKM) {
+        this.hinh = hinh;
+        this.sao = sao;
+        this.the = the;
+        this.maKM = maKM;
+        this.tenSan = tenSan;
+        this.khoangCach = khoangCach;
+        this.promo = promo;
+    }
+
+    public int getHinh() {
+        return hinh;
+    }
+
+    public int getSao() {
+        return sao;
+    }
+
+    public int getThe() {
+        return the;
+    }
+
+    public String getMaKM() {
+        return maKM;
+    }
+
+    public String getTenSan() {
+        return tenSan;
+    }
+
+    public String getKhoangCach() {
+        return khoangCach;
+    }
+
+    public String getPromo() {
+        return promo;
+    }
+
+    public void setHinh(int hinh) {
+        this.hinh = hinh;
+    }
+
+    public void setSao(int sao) {
+        this.sao = sao;
+    }
+
+    public void setThe(int the) {
+        this.the = the;
+    }
+
+    public void setMaKM(String maKM) {
+        this.maKM = maKM;
+    }
+
+    public void setTenSan(String tenSan) {
+        this.tenSan = tenSan;
+    }
+
+    public void setKhoangCach(String khoangCach) {
+        this.khoangCach = khoangCach;
+    }
+
+    public void setPromo(String promo) {
+        this.promo = promo;
+    }
+}
--- app/src/main/java/com/example/tennis_booking_app/Promotion.java
+++ app/src/main/java/com/example/tennis_booking_app/Promotion.java
@@ -5,10 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Promotion extends AppCompatActivity {
-
+    RecyclerView rvPromo;
+    ArrayList<PromoClass> arrPromo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
+        rvPromo=(RecyclerView) findViewById(R.id.rvPromo);
+        arrPromo=new ArrayList<>();
+
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                        "Promo","Sân Q9-Kim Sa","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Q7-Hoàng Sa","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Q7-Trường Sa","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Q9-Châu Sa","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Thủ Đức-Lam Sơn","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Thủ Đức-Nhật Tân","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Gò Vấp-Gia Định","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Gò Vấp-Nam Hà","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Q2-Đa màu","1km-4.8","REVLRA"));
+        arrPromo.add(new PromoClass(R.drawable.imgtennis1,R.drawable.ic_star_svgrepo_com,R.drawable.discount,
+                "Promo","Sân Q2-Chiến Thắng","1km-4.8","REVLRA"));
+
+        PromoAdapter adapter=new PromoAdapter(arrPromo);
+        rvPromo.setAdapter(adapter);
+        rvPromo.setLayoutManager(new LinearLayoutManager(this));
     }
 }
