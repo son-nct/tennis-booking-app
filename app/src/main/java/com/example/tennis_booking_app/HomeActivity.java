d4554809c510adc9931749cc9980f73b2c1626c9
phuchlh <hongphucthd@gmail.com>
2022-06-21 23:42:51 +0700

    [PhucHLH] some line of codes

--- app/src/main/java/com/example/tennis_booking_app/HomeActivity.java
+++ app/src/main/java/com/example/tennis_booking_app/HomeActivity.java
@@ -11,13 +11,17 @@ import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageView imgSearchBar;
+    ImageView imgSearchBar, imgHard, imgClay, imgGrass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         imgSearchBar = (ImageView) findViewById(R.id.imgSearchBar);
+        imgClay = (ImageView) findViewById(R.id.imgClay);
+        imgHard = (ImageView) findViewById(R.id.imgHard);
+        imgGrass = (ImageView) findViewById(R.id.imgGrass);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.parseColor("#AFF8A3"));
@@ -27,6 +31,33 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeActivity.this, SearchPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SpecificCourtsActivity.class);
+                intent.putExtra("Type", "Hard");
+                startActivity(intent);
+            }
+        });
+
+        imgClay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SpecificCourtsActivity.class);
+                intent.putExtra("Type", "Clay");
+                startActivity(intent);
+            }
+        });
+
+        imgGrass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SpecificCourtsActivity.class);
+                intent.putExtra("Type", "Grass");
                 startActivity(intent);
             }
         });
