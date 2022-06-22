52cf785e6155f8ed672ddaec3d13010e3187699c
phuchlh <hongphucthd@gmail.com>
2022-06-22 14:39:54 +0700

    [PhucHLH] bonus intent

--- app/src/main/java/com/example/tennis_booking_app/SpecificCourtsActivity.java
+++ app/src/main/java/com/example/tennis_booking_app/SpecificCourtsActivity.java
@@ -2,13 +2,30 @@ package com.example.tennis_booking_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SpecificCourtsActivity extends AppCompatActivity {
+
+    TextView txtType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_courts);
+
+        txtType = findViewById(R.id.txtType);
+
+        Intent intent = getIntent();
+        String noiDung = intent.getStringExtra("Type");
+
+        if(noiDung.equals("Hard")){
+            txtType.setText("Hard courts");
+        }else if(noiDung.equals("Grass")){
+            txtType.setText("Grass courts");
+        }else if(noiDung.equals("Clay")){
+            txtType.setText("Clay courts");
+        }
     }
 }
