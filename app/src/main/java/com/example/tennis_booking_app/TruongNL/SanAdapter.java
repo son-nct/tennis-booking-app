6f3e9d047e3ff64796a0751a730f639106f04783
SE140774\Administrator <truong>
2022-07-06 18:12:10 +0700

    [truongnl]test nhanh cu

---
+++ app/src/main/java/com/example/tennis_booking_app/TruongNL/SanAdapter.java
@@ -0,0 +1,65 @@
+package com.example.tennis_booking_app.TruongNL;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tennis_booking_app.R;
+
+import java.util.ArrayList;
+
+public class SanAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private ArrayList<SanTennis> sanTennisList;
+
+    public SanAdapter(Context context, int layout, ArrayList<SanTennis> sanTennisList) {
+        this.context = context;
+        this.layout = layout;
+        this.sanTennisList = sanTennisList;
+    }
+
+    @Override
+    public int getCount() {
+        return sanTennisList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView=inflater.inflate(layout,null);
+
+        ImageView imgHinh=(ImageView) convertView.findViewById(R.id.imgHinh);
+        TextView txtTen=(TextView) convertView.findViewById(R.id.txtTensan);
+        TextView txtDientich=(TextView) convertView.findViewById(R.id.txtDientich);
+        TextView txtTien=(TextView) convertView.findViewById(R.id.txtTien);
+        TextView txtKhoangCach =(TextView) convertView.findViewById(R.id.txtKhoangCach);
+        TextView txtRating =(TextView) convertView.findViewById(R.id.txtRating);
+
+        SanTennis san= sanTennisList.get(position);
+
+        imgHinh.setImageResource(san.getHinh());
+        txtTen.setText(san.getTen());
+        txtDientich.setText(san.getDientich());
+        txtTien.setText(san.getTien());
+        txtKhoangCach.setText(san.getKhoangCach());
+        txtRating.setText(san.getRating());
+
+        return convertView;
+    }
+}
