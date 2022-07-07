8ef65993033aa07eb752130563ba9ce79c1e7360
Hunvy <henri.deaaron@gmail.com>
2022-07-07 23:49:06 +0700

    Auto stash before merge of "release/29-07-2022" and "origin/release/29-07-2022"

--- app/src/main/java/com/example/tennis_booking_app/ThinhLTD/Register.java
+++ app/src/main/java/com/example/tennis_booking_app/ThinhLTD/Register.java
@@ -3,9 +3,12 @@ package com.example.tennis_booking_app.ThinhLTD;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -31,99 +34,115 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        changeStatusBarColor();
 
-        txtUsername = findViewById(R.id.txtUsername);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtPassword = findViewById(R.id.txtPassword);
-        txtConfirm = findViewById(R.id.txtConfirm);
-        txtFullname = findViewById(R.id.txtFullname);
-        txtPhone = findViewById(R.id.txtPhone);
-        chkCheck = findViewById(R.id.chkCheck);
-        btnRegister = findViewById(R.id.btnRegister);
+//        txtUsername = findViewById(R.id.textInputName);
+//        txtEmail = findViewById(R.id.textInputEmail);
+//        txtPassword = findViewById(R.id.textInputPassword);
+////        txtConfirm = findViewById(R.id.txtConfirm);
+////        txtFullname = findViewById(R.id.txtFullname);
+////        txtPhone = findViewById(R.id.txtPhone);
+////        chkCheck = findViewById(R.id.chkCheck);
+//        btnRegister = findViewById(R.id.btnRegister);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(checkCondition()){
-                    register();
-                }else{
-                    Toast.makeText(Register.this, "Input your empty field and try again", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        btnRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (checkCondition()) {
+//                    register();
+//                } else {
+//                    Toast.makeText(Register.this, "Input your empty field and try again", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
     }
 
-    private void getData(){
-        username = txtUsername.getText().toString();
-        email = txtEmail.getText().toString();
-        password = txtPassword.getText().toString();
-        confirmPass = txtConfirm.getText().toString();
-        fullname = txtFullname.getText().toString();
-        phone = txtPhone.getText().toString();
+//    private void getData() {
+//        username = txtUsername.getText().toString();
+//        email = txtEmail.getText().toString();
+//        password = txtPassword.getText().toString();
+//        confirmPass = txtConfirm.getText().toString();
+//        fullname = txtFullname.getText().toString();
+//        phone = txtPhone.getText().toString();
+//    }
+
+//    private void register() {
+//        RegisterRequest registerRequest = new RegisterRequest();
+//        registerRequest.setUsername(username);
+//        registerRequest.setEmail(email);
+//        registerRequest.setPassword(password);
+//        registerRequest.setConfirmPassword(confirmPass);
+//        registerRequest.setFullName(fullname);
+//        registerRequest.setPhoneNumber(phone);
+//        registerRequest.setClientID(3);
+//
+//        Call<RegisterResponse> registerResponseCall = ApiClient.getUserService().userRegister(registerRequest);
+//        registerResponseCall.enqueue(new Callback<RegisterResponse>() {
+//            @Override
+//            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
+//                if (response.isSuccessful()) {
+//                    Toast.makeText(Register.this, response.message(), Toast.LENGTH_SHORT).show();
+//                    RegisterResponse registerResponse = response.body();
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            Intent intent = new Intent(Register.this, StartsActivity.class);
+//                            startActivity(intent);
+//                        }
+//                    }, 700);
+//                } else {
+//                    Toast.makeText(Register.this, "Failed", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<RegisterResponse> call, Throwable t) {
+//                Toast.makeText(Register.this, "Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+//    }
+//
+//
+//    private boolean checkCondition() {
+//        getData();
+//        if (username.length() == 0) {
+//            setError(txtUsername, "Username không được để trống");
+//            return false;
+//        } else if (email.length() == 0) {
+//            setError(txtEmail, "Email không được để trống");
+//            return false;
+//        } else if (password.length() == 0) {
+//            setError(txtPassword, "Mật khẩu không được để trống");
+//            return false;
+//        } else if (confirmPass.length() == 0 && !password.equals(confirmPass)) {
+//            setError(txtConfirm, "Nhập lại mật khẩu không chính xác");
+//            return false;
+//        } else if (fullname.length() == 0) {
+//            setError(txtConfirm, "Tên đầy đủ không được để trống");
+//            return false;
+//        } else if (phone.length() == 0) {
+//            setError(txtConfirm, "Số điện thoại không được để trống");
+//            return false;
+//        } else return true;
+//    }
+
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+        }
     }
 
-    private void register(){
-        RegisterRequest registerRequest = new RegisterRequest();
-        registerRequest.setUsername(username);
-        registerRequest.setEmail(email);
-        registerRequest.setPassword(password);
-        registerRequest.setConfirmPassword(confirmPass);
-        registerRequest.setFullName(fullname);
-        registerRequest.setPhoneNumber(phone);
-        registerRequest.setClientID(3);
+    public void onLoginClick(View view) {
+        startActivity(new Intent(this, StartsActivity.class));
+        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
 
-        Call<RegisterResponse> registerResponseCall = ApiClient.getUserService().userRegister(registerRequest);
-        registerResponseCall.enqueue(new Callback<RegisterResponse>() {
-            @Override
-            public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
-                if(response.isSuccessful()){
-                    Toast.makeText(Register.this, response.message(), Toast.LENGTH_SHORT).show();
-                    RegisterResponse registerResponse = response.body();
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            Intent intent = new Intent(Register.this, StartsActivity.class);
-                            startActivity(intent);
-                        }
-                    }, 700);
-                }else{
-                    Toast.makeText(Register.this, "Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RegisterResponse> call, Throwable t) {
-                Toast.makeText(Register.this, "Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
     }
 
-
-    private boolean checkCondition(){
-        getData();
-        if(username.length() == 0){
-            setError(txtUsername, "Username không được để trống");
-            return false;
-        }else if(email.length()== 0){
-            setError(txtEmail, "Email không được để trống");
-            return false;
-        }else if(password.length() == 0){
-            setError(txtPassword, "Mật khẩu không được để trống");
-            return false;
-        }else if(confirmPass.length() == 0 && !password.equals(confirmPass)){
-            setError(txtConfirm, "Nhập lại mật khẩu không chính xác");
-            return false;
-        }else if(fullname.length() == 0){
-            setError(txtConfirm, "Tên đầy đủ không được để trống");
-            return false;
-        }else if(phone.length() == 0){
-            setError(txtConfirm, "Số điện thoại không được để trống");
-            return false;
-        }else return true;
-    }
-
-    private void setError(EditText input, String s){
+    private void setError(EditText input, String s) {
         input.setError(s);
         input.requestFocus();
     }
--- app/src/main/java/com/example/tennis_booking_app/ThinhLTD/StartsActivity.java
+++ app/src/main/java/com/example/tennis_booking_app/ThinhLTD/StartsActivity.java
@@ -33,71 +33,76 @@ public class StartsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starts);
 
-        etUsername = (EditText) findViewById(R.id.txtUsername);
-        etPassword = (EditText) findViewById(R.id.txtPassword);
-        btnContinue = (Button) findViewById(R.id.btnContinue);
-        tvforgotPassword = (TextView) findViewById(R.id.tvforgotPassword);
-
-        tvforgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartsActivity.this, Retrieval.class);
-                startActivity(intent);
-            }
-        });
+//        etUsername = (EditText) findViewById(R.id.textInputName);
+//        etPassword = (EditText) findViewById(R.id.textInputPassword);
+//        btnContinue = (Button) findViewById(R.id.btnLogin);
+//        tvforgotPassword = (TextView) findViewById(R.id.);
+//
+//        tvforgotPassword.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(StartsActivity.this, Retrieval.class);
+//                startActivity(intent);
+//            }
+//        });
         //clicktieptuc
 
-        btnContinue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(etUsername.getText().toString()) || TextUtils.isEmpty(etPassword.getText().toString())) {
-                    Toast.makeText(StartsActivity.this, "Username / Password Required", Toast.LENGTH_LONG).show();
-                } else {
-                    login();
-                }
-            }
-        });
+//        btnContinue.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (TextUtils.isEmpty(etUsername.getText().toString()) || TextUtils.isEmpty(etPassword.getText().toString())) {
+//                    Toast.makeText(StartsActivity.this, "Username / Password Required", Toast.LENGTH_LONG).show();
+//                } else {
+//                    login();
+//                }
+//            }
+//        });
 
     }
 
-    public void login() {
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setUsername(etUsername.getText().toString());
-        loginRequest.setPassword(etPassword.getText().toString());
-        loginRequest.setClientId(3);
-        loginRequest.setAutoSignIn(true);
+//    public void login() {
+//        LoginRequest loginRequest = new LoginRequest();
+//        loginRequest.setUsername(etUsername.getText().toString());
+//        loginRequest.setPassword(etPassword.getText().toString());
+//        loginRequest.setClientId(3);
+//        loginRequest.setAutoSignIn(true);
+//
+//        Call<LoginResponse> loginResponseCall = ApiClient.getUserService().userLogin(loginRequest);
+//        loginResponseCall.enqueue(new Callback<LoginResponse>() {
+//            @Override
+//            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+//
+//                if (response.isSuccessful()) {
+//                    Toast.makeText(StartsActivity.this, response.body().getMessage(), Toast.LENGTH_LONG).show();
+//                    LoginResponse loginResponse = response.body();
+//                    System.out.println(loginResponse);
+//                    new Handler().postDelayed(new Runnable() {
+//                        @Override
+//                        public void run() {
+////                            startActivity(new Intent(StartsActivity.this, HomeActivity.class)putExtra("data", loginResponse.getToken().getUserName()));
+//                            Intent intent = new Intent(StartsActivity.this, HomeActivity.class);
+//                            intent.putExtra("data", loginResponse.getToken().getFullName());
+//                            startActivity(intent);
+//                        }
+//                    }, 700);
+//
+//                } else {
+//                    Toast.makeText(StartsActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<LoginResponse> call, Throwable t) {
+//                Toast.makeText(StartsActivity.this, "Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//
+//    }
 
-        Call<LoginResponse> loginResponseCall = ApiClient.getUserService().userLogin(loginRequest);
-        loginResponseCall.enqueue(new Callback<LoginResponse>() {
-            @Override
-            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-
-                if (response.isSuccessful()) {
-                    Toast.makeText(StartsActivity.this, response.body().getMessage(), Toast.LENGTH_LONG).show();
-                    LoginResponse loginResponse = response.body();
-                    System.out.println(loginResponse);
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-//                            startActivity(new Intent(StartsActivity.this, HomeActivity.class)putExtra("data", loginResponse.getToken().getUserName()));
-                            Intent intent = new Intent(StartsActivity.this, HomeActivity.class);
-                            intent.putExtra("data", loginResponse.getToken().getFullName());
-                            startActivity(intent);
-                        }
-                    }, 700);
-
-                } else {
-                    Toast.makeText(StartsActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<LoginResponse> call, Throwable t) {
-                Toast.makeText(StartsActivity.this, "Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-            }
-        });
-
-
+    public void onLoginClick(View View) {
+        startActivity(new Intent(this, HomeActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
